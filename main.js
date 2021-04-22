f9500ff16164d24a4f4613da0dd14ba95d7fb86f
Michael MacPherson <schmyesh@gmail.com>
2021-04-21 22:15:48 -0500

    test4

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ try {
 1 ==== 2;
 display_notice("Nexus community basher has loaded successfully","yellow");
 
+nb.alias = function(c) {
+	return false;
+}
+
+nb.trigger = function(c) {
+	return false;
+}
+
 } catch (err) {
 	display_notice("Nexus community basher has failed to load. Check console for error message.","red");
 	console.log(err);
