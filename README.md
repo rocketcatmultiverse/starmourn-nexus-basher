98dc2a987a9f3656246d89ea7f40139c6bb5270b
Michael MacPherson <schmyesh@gmail.com>
2021-04-22 05:54:11 -0500

    Soza's leveling guide mob adds

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ This project is a community autobasher for the IRE game Starmourn.
 
 ## installation
 
-Download the [latest Release of the .nxs package](https://github.com/rocketcatmultiverse/starmourn-nexus-basher/releases), head into Nexus, click "Settings" in the bottom right, tab to "reflex packages". At the bottom you will see a box which says "Click here or drop a file with a reflex package to load it." Click and browse or drop the downloaded .nxs. You're done!
+Download the [latest Release of the .nxs package](https://github.com/rocketcatmultiverse/starmourn-nexus-basher/releases), head into Nexus, click "Settings" in the bottom right, tab to "reflex packages". At the bottom you will see a box which says "Click here or drop a file with a reflex package to load it." Click and browse or drop the downloaded .nxs. You're done! If you do not see "Welcome to the Starmourn Community Nexus Basher!", you may need to restart your client or type onLoad (case sensitive).
 
 ## usage
 
@@ -12,3 +12,9 @@ Type NBHELP in game for usage instructions.
 ## contributing
 
 Contributions are most welcome! Send your PRs! If you're not confident with git but have suggestions or even code you'd like to share, please do!
+
+## tips
+
+Invest your first 50 stats into lifeforce. After that, invest your stats into Regen until Regen is maxed. Next, invest stats into your primary damage stat, depending on what class you are, until it is maxed. Finally, the remaining stats can go to your lifeforce. Things can change when you hit end-game and if you're interested in pvp, but this is a stable set up!
+
+If you're unsure where to bash, follow [this leveling guide](https://forums.starmourn.com/discussion/1257/sozas-leveling-guide-and-newbiechallenge-guide#latest)! 
--- mobs.js
+++ mobs.js
@@ -1,9 +1,56 @@
 nb.mobs = [
+	//====================
+	// 1-15
+	//====================
 	//Southern Usum Usutti
 	"a yellow-feathered spiderax","a spotted mouse","a sleek shadow fox",
 	"a submerged tentacle","a medium-sized tentacle","a red-backed water bug",
 	//Marle
 	"a Kronish Cross rockhopper","a large Kronish Cross rockhopper","a jumbo Kronish Cross rockhopper",
+	//Mirror Lake
+	"a water skipper","a sparkling eel","a small arboreal lizard",
+	//Dregs
+	"a miniature mauve rodent","a hairless vermilion rodent","a vicious vermilion rodent","a ravenous red rodent",
+
+	//====================
+	// 10-20
+	//====================
+	//Oldtown/Lower reaches/Undercity
+	"a glitch-riddled security robot", "an aggressively malfunctioning drone",
+	//Coriolis
+	"a malfunctioning windmill drone","a horned lapine","a venomous copper flitwing",
+	//Scrapston
+	"a grime-slick eel","a needle-mouthed eel","a poisonous eel",
+	//Whittler's hollow
+	"a fat terraworm","a meaty terraworm","an iridescent opal beetle", "a horrifically mutated rodent",
+	//Tabby
+	"a grotesquely mutated bird", "a bile-skinned Amaian mutant", "a green-glowing Decheeran mutant", "a scab-scaled Ry'nari mutant",
+    "a grotesque Nusriza mutant", "a mutant encased in ancient power armor", "a warty Ry'nari mutant", "a tatter-finned Amaian mutant",
+    "a twisted Decheeran mutant",
+
+	//====================
+	// 20-40
+	//====================
+    //Goribar
+    "a small loam leech", "a saffron algerion", "a coral algerion", "a moss-hued algerion", "a large loam leech",
+    "an azure algerion", "an oversized loam leech", "a loam leech",
+
+	//====================
+	// 40-50
+	//====================
+	//Pylos
+    "a tentacle-finned hookmaw", "a slow-moving hunk of living coral", "an algae-covered calciburr",
+    "a depthless puddle of black liquid", "a poison-spitting treefrog","a delicately bulbous blinkfish",
+
+
+	//====================
+	// 50-75
+	//====================
+	//New Dikamazi
+	"a rampaging construction mech","a dilapidated construction mech","a cyborg leech orchid",
+	//Ixsei
+	"a crystal-clawed rock giant","a salt-crusted quartz creeper",
+
 ];
 
 nb.ignores = [];
