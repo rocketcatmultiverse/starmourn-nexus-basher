59f11e7b35ccce9bcc9584c47ef3c87da797d7b4
Michael MacPherson <schmyesh@gmail.com>
2021-04-22 05:25:02 -0500

    Marle mobs

--- mobs.js
+++ mobs.js
@@ -2,6 +2,8 @@ nb.mobs = [
 	//Southern Usum Usutti
 	"a yellow-feathered spiderax","a spotted mouse","a sleek shadow fox",
 	"a submerged tentacle","a medium-sized tentacle","a red-backed water bug",
+	//Marle
+	"a Kronish Cross rockhopper","a large Kronish Cross rockhopper","a jumbo Kronish Cross rockhopper",
 ];
 
 nb.ignores = [];
