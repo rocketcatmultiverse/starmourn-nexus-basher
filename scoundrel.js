40aa596bbc5c4e0c190068699a8357abb18253ff
Michael MacPherson <schmyesh@gmail.com>
2021-04-23 07:13:05 -0500

    haveSkill fixes

--- scoundrel.js
+++ scoundrel.js
@@ -1,8 +1,8 @@
 var have = nb.haveSkill;
 
 nb.offense.Scoundrel = function(){
-	var fling = have("improvisation","fling");
-	var eject = fling && nb.tarHealth > 20 && nb.bullets === 8 && have("gunslinging","eject");
+	var fling = nb.haveSkill("improvisation","fling");
+	var eject = fling && nb.tarHealth > 20 && nb.bullets === 8 && nb.haveSkill("gunslinging","eject");
 	if (nb.bullets === 0 || eject) {
 		if (fling) {
 			if (eject) nb.send("gun eject");
@@ -11,11 +11,11 @@ nb.offense.Scoundrel = function(){
 			return "gun quickload";
 		}
 	}
-	var rf = have("gunslinging","rapidfire") && !("ab_Gunslinging_rapidfire" in nb.cooldowns);
+	var rf = nb.haveSkill("gunslinging","rapidfire") && !("ab_Gunslinging_rapidfire" in nb.cooldowns);
 	if (nb.bullets == 2 && rf) {
 		return "gun rapidfire "+nb.tar;
 	}
-	if (have("gunslinging","ambush") && !("ab_Gunslinging_ambush" in nb.cooldowns)) {
+	if (nb.haveSkill("gunslinging","ambush") && !("ab_Gunslinging_ambush" in nb.cooldowns)) {
 		return eject+"gun ambush "+nb.tar;
 	}
 	if (rf) {
@@ -28,8 +28,8 @@ nb.offense.Scoundrel = function(){
 nb.wantedIED = "";
 nb.scoundrelGetIEDFling = function() {
 	var ied = ""
-	var mod = have("improvisation","shrapnel") ? "shrapnel " : "";
-	if (tarHealth > 30 && have("improvisation","melter")) {
+	var mod = nb.haveSkill("improvisation","shrapnel") ? "shrapnel " : "";
+	if (tarHealth > 30 && nb.haveSkill("improvisation","melter")) {
 		ied = "melter";
 	} else {
 		ied = "ripper"; //don't need a skill check, we always have rippers if we have fling.
