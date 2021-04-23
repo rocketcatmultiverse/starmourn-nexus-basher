a82485371de18d7265d0da272cf755d152af17cc
Michael MacPherson <schmyesh@gmail.com>
2021-04-23 06:51:17 -0500

    learning abs reload

--- trigger.js
+++ trigger.js
@@ -5,5 +5,8 @@ nb.trigger = function(c) {
 		nb.tarCheck();
 	}
 	else if (nb.isInterruptLine(c)) nb.interrupt = true;
+	else if (c.includes("You have learned the following abilities in this session")) {
+		display_notice("We notice you are gaining new skills. When you are finished learning, NBRELOAD so that Nexus Basher uses the best abilities", "green");
+	}
 	return false;
 }
