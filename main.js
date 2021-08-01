5f3280f234887f65b688dfe6a9b6245bb5d5e6c2
zombilicious <46409147+zombilicious@users.noreply.github.com>
2021-08-01 10:37:39 -0400

    Update main.js

--- main.js
+++ main.js
@@ -184,24 +184,30 @@ nb.needInterrupt = function () {
         return false;
     switch (nb.class) {
     case "Engineer":
-        return "bot swing " + nb.tar;
+        nb.interrupt = false;
+        return "bot swing " + nb.chanTar;
     case "Scoundrel":
+        nb.interrupt = false;
         if (nb.bullets === 0) {
             return "guile pocketsand " + nb.tar;
         } else {
             return "gun pointblank " + nb.tar;
         }
     case "BEAST":
+        nb.interrupt = false;
         //if (nb.mwpActive("netlauncher")) return "netlaunch "+nb.tar; //this would be better
         if (nb.haveSkill("mwp", "dualshot") && !("ab_MWP_netlaunch" in nb.cooldowns))
             return "netlaunch " + nb.tar;
         return "plasma flash " + nb.tar;
     case "Fury":
+        nb.interrupt = false;
         return "kith fever " + nb.tar;
     case "Nanoseer":
+        nb.interrupt = false;
         //return "nano eyestrike " + nb.tar;
         return "nano eyestrike " + nb.chanTar;
     default:
+        nb.interrupt = false;
         nb.error("Invalid class " + nb.class + " provided to nb.sendHeal");
         return false;
     }
