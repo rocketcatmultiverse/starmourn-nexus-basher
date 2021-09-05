9ebf3f492a5e8888985f7cf01e44762aa20466a0
Diego Castro <37005429+diegojcastro@users.noreply.github.com>
2021-09-04 20:52:58 -0400

    move nb.interrupt flag until after IH parses
    Moved the line to send the interrupt command to main.js until after IH has parsed and captured the right target.

--- trigger.js
+++ trigger.js
@@ -8,7 +8,7 @@ nb.trigger = function(c) {
 		nb.onKill();
 	}
 	else if (nb.isInterruptLine(c)) {
-		nb.interrupt = true;
+		// nb.interrupt = true; // moving to line 49
 		nb.send("ih");
 	} else if (c.includes("You have learned the following abilities in this session")) {
 		display_notice("We notice you are gaining new skills. When you are finished learning, NBRELOAD so that Nexus Basher uses the best abilities", "green");
@@ -47,6 +47,7 @@ nb.trigger = function(c) {
 	}else if ((xyz = nb.interruptRegex.exec(c)) !== null) {
 		nb.debug("Interrupting mob "+JSON.stringify(xyz));
 		nb.chanTar = xyz[1];
+		nb.interrupt = true; // moved from line 11
 		nb.hider();
 	} else if (c.includes("Items here:")) {
                 nb.hideIH = true;
