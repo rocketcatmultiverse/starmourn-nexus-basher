5e0b4e09e23acf42fd8103eae584cf05bed4d853
rocketcatmultiverse <schmyesh@gmail.com>
2021-09-22 20:00:27 +0000

    shock touchup

--- engineer.js
+++ engineer.js
@@ -2,8 +2,8 @@ nb.needQPCBoost = false;
 nb.degaussed = false;
 nb.offense.Engineer = function(){
 	if (!nb.configs.no_qpcboost.val && nb.needQPCBoost) return 'qpcboost';
-	if (nb.configs.use_shock_rotation) {
-		if (nb.configs.degauss_turret && !nb.degaussed && nb.tarHealth < 15) return "gadget degauss "+nb.tar+" turret";
+	if (nb.configs.use_shock_rotation.val) {
+		//if (nb.configs.degauss_turret.val && !nb.degaussed && nb.tarHealth < 15) return "gadget degauss "+nb.tar+" turret";
 		return "gadget shock "+nb.tar;
 	}
 	return "bot claw "+nb.tar;
