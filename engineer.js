34851e674b83c2f00211c9fc79cae7232039be3a
rocketcatmultiverse <82978996+rocketcatmultiverse@users.noreply.github.com>
2022-02-05 14:06:47 -0600

    add bot pulse

--- engineer.js
+++ engineer.js
@@ -2,6 +2,7 @@ nb.needQPCBoost = false;
 nb.degaussed = false;
 nb.offense.Engineer = function(){
 	if (!nb.configs.no_qpcboost.val && nb.needQPCBoost) return 'qpcboost';
+	if (nb.tarIsMech && nb.haveSkill("bots","pulse")) { return "bot pulse "+nb.tar; }
 	if (nb.configs.use_shock_rotation.val) {
 		//if (nb.configs.degauss_turret.val && !nb.degaussed && nb.tarHealth < 15) return "gadget degauss "+nb.tar+" turret";
 		return "gadget shock "+nb.tar;
